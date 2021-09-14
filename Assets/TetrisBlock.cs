f228e66353dc6c80d10efa211e064355d3875502
n2sjp <nord2sudjp@gmail.com>
2021-09-14 22:09:46 +0900

    Tetromino einshrank in Displya

--- Assets/TetrisBlock.cs
+++ Assets/TetrisBlock.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
 {
+
+    private float prevTime;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +22,41 @@ public class TetrisBlock : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (!ValidMove()) {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
-        }        
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+
+        } else if ((Time.time - prevTime) > (Input.GetKey(KeyCode.DownArrow) ? fallTime/10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+
+            prevTime = Time.time;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform){
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height){
+                string str = "Invalid:" + roundedX.ToString() +","+ roundedY.ToString();
+                Debug.Log(str);
+                return false;                
+            }
+        }
+        return true;
     }
 }
